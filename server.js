e7bdc5392448bb593927b6855f5e520d6923e2af
jessnng <jnnguy40@cougarnet.uh.edu>
2024-03-27 21:40:32 -0500

    restored server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const bcrypt = require('bcrypt');
-const { authRegisterController } = require('/controllers/server');
+//const { authRegisterController } = require('/controllers/server');
 
 // allows access to files in folder
 app.use(express.static(path.join(__dirname, 'pages')));
@@ -65,7 +65,28 @@ app.post('/login', async (req, res) => {
 });
 
 // REGISTER USER ROUTE
-app.post('/register', authRegisterController);
+app.post('/register', async (req, res) => {
+  try{
+    const { username, password } = req.body;
+
+    const db = client.db("appdb");
+    const collection = db.collection("users");
+
+    const existingUser = await collection.findOne({ username });
+    if (existingUser) {
+      res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Encrypt password
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 is the saltRounds
+    await collection.insertOne({ username, password: hashedPassword });
+    
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to register user" });
+  }
+});
 
 // Create user profile route
 app.post('/profile/:username/update', async (req, res) => {
@@ -163,5 +184,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-export default server
