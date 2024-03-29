c5bed99a0a13c0b353fa279019c29ad90502331d
Andrew Guzman <104712601+Andrew02g@users.noreply.github.com>
2024-03-29 15:03:35 -0500

    Create quoteForm.test.js
    Signed-off-by: Andrew Guzman <104712601+Andrew02g@users.noreply.github.com>

---
+++ __tests__/quoteForm.test.js
@@ -0,0 +1,49 @@
+
+
+const mockUserData = {
+    body: {
+        fullname: 'Jane Doe',
+        address1: '123 St',
+        city: 'Houston',
+        state: 'TX',
+        zipcode: '12345'
+    }
+};
+
+const quoteMockData = {
+    body: {
+        gallonsRequest: 16,
+        deliveryAddress: mockUserData.body,
+        deliveryDate: '3/31/2023',
+        suggestedPrice: 2.85,
+        amountDue: 45.6
+    }
+};
+
+// Quote Form - POST Request
+describe('POST, /quote-form - send quote form to server', () => {
+
+    it('should send status 400 when missing gallons request', async () => {
+        await quoteFormController(quoteMockData.body.gallonsRequest, mockRespond);
+        if (!quoteMockData.body.gallonsRequest){
+            expect(mockRespond.status).toHaveBeenCalledWith(400);
+            expect(mockRespond.json).toHaveReturnedWith('A value is needed for gallons requested.')
+        }
+                
+    })
+
+    it('should send status 200 when succesfully send form', async () => {
+        await quoteFormController(quoteMockData.body, mockRespond);
+        if(quoteMockData.body.gallonsRequest) {
+            expect(quoteMockData.body).toHaveProperty('gallonsRequest', 16);
+        }
+        
+    });
+
+    it('should send status 500 when failed to submit form, and when missing value', async () => {
+        await quoteFormController({}, mockRespond);
+        expect(mockRespond.status).toHaveBeenCalledWith(500);
+        expect(mockRespond.json).toHaveReturnedWith('Internal server error.');
+    })
+
+});
