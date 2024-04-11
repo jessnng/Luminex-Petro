11e422b45713099e95ee06f23f4e2662b143cb5b
Andrew Guzman <104712601+Andrew02g@users.noreply.github.com>
2024-04-10 21:21:10 -0500

    Update registerUser.js
    Signed-off-by: Andrew Guzman <104712601+Andrew02g@users.noreply.github.com>

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -1,5 +1,5 @@
 //const { authRegisterController } = require('../controllers/server.js');
-
+/*
 const bcrypt = require('bcrypt');
 
 // CONNECT TO MONGODB ATLAS
@@ -55,4 +55,47 @@ const authRegisterController = async(req, res) => {
   }
 };
 
+module.exports = { authRegisterController };  */
+
+const bcrypt = require('bcrypt');
+
+// Mock MongoDB client for testing purposes
+const mockDBClient = {
+  db: () => ({
+    collection: () => ({
+      findOne: jest.fn(),
+      insertOne: jest.fn(),
+    }),
+  }),
+};
+
+const authRegisterController = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    const db = mockDBClient.db("appdb");
+    const collection = db.collection("users");
+
+    const existingUser = await collection.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json("Username already exists");
+    }
+
+    // Encrypt password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await collection.insertOne({ username, password: hashedPassword });
+
+    // Redirect user to Profile.html upon successful registration
+    res.status(201).json({ redirectTo: '/Profile.html' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Failed to register user" });
+  }
+};
+
 module.exports = { authRegisterController };
+
